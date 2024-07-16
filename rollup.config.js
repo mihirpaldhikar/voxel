3890a77659250cbffccffcbc4a53a9ca31e194e1
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-07-16 15:15:31 +0530

    build: Add rollup for bundling all the source files to generate optimized outputs along with type declarations which can be published to NPM registry.

---
+++ rollup.config.js
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) Mihir Paldhikar
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+import {readFileSync} from "fs";
+import pluginPeerDepsExternalModule from "rollup-plugin-peer-deps-external";
+import terser from "@rollup/plugin-terser";
+
+const packageJsonURL = new URL("./package.json", import.meta.url);
+const packageJson = JSON.parse(readFileSync(packageJsonURL, "utf8"));
+
+const rollupConfiguration = [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: packageJson.module,
+                format: "esm",
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            postcss({
+                extract: false
+            }),
+            resolve({
+                extensions: [".css"]
+            }),
+            commonjs({
+                transformMixedEsModules: true,
+                include: []
+            }),
+            pluginPeerDepsExternalModule(),
+            typescript({
+                tsconfig: "./tsconfig.json",
+                declaration: true,
+                declarationDir: 'dist',
+                exclude: ["**/tests/", "**/*.test.ts", "**/*.test.tsx"]
+            }),
+            terser()
+        ]
+    },
+    {
+        input: "dist/esm/index.d.ts",
+        output: [{file: "dist/index.d.ts", format: "esm"}],
+        plugins: [dts()],
+        external: [/\.css$/]
+    }
+];
+
+export default rollupConfiguration;
