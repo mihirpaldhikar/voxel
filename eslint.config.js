17bc20fd4ae86a6553b9d433461689f43de5a155
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-07-16 14:27:31 +0530

    build: Configure ESLint for linting errors and warning inorder to write robust code and follow best practices.

---
+++ eslint.config.js
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) Mihir Paldhikar
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+
+export default [
+    {
+        files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
+    },
+    {
+        rules: {
+            "@typescript-eslint/no-non-null-assertion": "off",
+            "no-empty": "error",
+            "no-empty-function": "error",
+            "no-unused-vars": [
+                "error",
+                {
+                    "args": "after-used",
+                    "argsIgnorePattern": "^_"
+                }
+            ]
+        }
+    },
+    {
+        languageOptions: {
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true
+                }
+            }
+        }
+    },
+    {
+        languageOptions: {
+            globals: globals.browser
+        }
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReactConfig,
+];
