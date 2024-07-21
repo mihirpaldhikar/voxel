8e7686eba1e7d4be04dac20a6690750e9ea6db48
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-07-21 18:22:25 +0530

    style: Format files with Prettier.

--- rollup.config.js
+++ rollup.config.js
@@ -25,7 +25,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
-import {readFileSync} from "fs";
+import { readFileSync } from "fs";
 import pluginPeerDepsExternalModule from "rollup-plugin-peer-deps-external";
 import terser from "@rollup/plugin-terser";
 
@@ -33,42 +33,42 @@ const packageJsonURL = new URL("./package.json", import.meta.url);
 const packageJson = JSON.parse(readFileSync(packageJsonURL, "utf8"));
 
 const rollupConfiguration = [
-    {
-        input: "src/index.ts",
-        output: [
-            {
-                file: packageJson.module,
-                format: "esm",
-                sourcemap: true
-            }
-        ],
-        plugins: [
-            postcss({
-                extract: false
-            }),
-            resolve({
-                extensions: [".css"]
-            }),
-            commonjs({
-                transformMixedEsModules: true,
-                include: []
-            }),
-            pluginPeerDepsExternalModule(),
-            typescript({
-                tsconfig: "./tsconfig.json",
-                declaration: true,
-                declarationDir: 'dist',
-                exclude: ["**/tests/", "**/*.test.ts", "**/*.test.tsx"]
-            }),
-            terser()
-        ]
-    },
-    {
-        input: "dist/esm/index.d.ts",
-        output: [{file: "dist/index.d.ts", format: "esm"}],
-        plugins: [dts()],
-        external: [/\.css$/]
-    }
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      postcss({
+        extract: false
+      }),
+      resolve({
+        extensions: [".css"]
+      }),
+      commonjs({
+        transformMixedEsModules: true,
+        include: []
+      }),
+      pluginPeerDepsExternalModule(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        declaration: true,
+        declarationDir: "dist",
+        exclude: ["**/tests/", "**/*.test.ts", "**/*.test.tsx"]
+      }),
+      terser()
+    ]
+  },
+  {
+    input: "dist/esm/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.css$/]
+  }
 ];
 
 export default rollupConfiguration;
